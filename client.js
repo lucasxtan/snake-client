0f11e7b27c30d9c5e355070e70331fe2b12672f6
Lucas Tan <sheng.x.tan@gmail.com>
2022-03-16 21:45:07 +0000

    add client.js, and exported and imported with ES6 shorthand syntax on play.js

---
+++ client.js
@@ -0,0 +1,26 @@
+const net = require("net");
+
+const connect = function () {
+  const conn = net.createConnection({
+    host: '165.227.47.243',
+    port: 50541
+  });
+
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+
+  conn.on("data", (data) => {
+    console.log('message from server:', data);
+  });
+
+  conn.on("connect", () => {
+    console.log('connected to the server');
+  });
+
+  
+  return conn;
+};
+
+module.exports = {
+  connect
+}
--- play.js
+++ play.js
@@ -1,30 +1,31 @@
 //this file is where you launch your game client
 
 //the client you build will connect to a game server ran by LHL
+const {connect} = require('./client.js');
 
 const net = require("net");
 
 // establishes a connection with the game server
-const connect = function () {
-  const conn = net.createConnection({
-    host: '165.227.47.243',
-    port: 50541
-  });
+// const connect = function () {
+//   const conn = net.createConnection({
+//     host: '165.227.47.243',
+//     port: 50541
+//   });
 
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
+//   // interpret incoming data as text
+//   conn.setEncoding("utf8");
 
-  conn.on("data", (data) => {
-    console.log('message from server:', data);
-  });
+//   conn.on("data", (data) => {
+//     console.log('message from server:', data);
+//   });
 
-  conn.on("connect", () => {
-    console.log('connected to the server');
-  });
+//   conn.on("connect", () => {
+//     console.log('connected to the server');
+//   });
 
-  
-  return conn;
-};
+
+//   return conn;
+// };
 
 console.log("Connecting ...");
 connect();
