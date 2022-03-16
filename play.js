6498f9d5e56802d28fac69dbc80de6c9fceeb783
Lucas Tan <sheng.x.tan@gmail.com>
2022-03-16 21:28:08 +0000

    add play.js

---
+++ play.js
@@ -0,0 +1,30 @@
+//this file is where you launch your game client
+
+//the client you build will connect to a game server ran by LHL
+
+const net = require("net");
+
+// establishes a connection with the game server
+const connect = function () {
+  const conn = net.createConnection({
+    host: '165.227.47.243',
+    port: 50541
+  });
+
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+
+  conn.on("data", (data) => {
+    console.log('message from server:', data);
+  });
+
+  conn.on("connect", () => {
+    console.log('connected to the server');
+  });
+
+  
+  return conn;
+};
+
+console.log("Connecting ...");
+connect();
